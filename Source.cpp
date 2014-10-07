83eed9c50c60de56d190516d88c2724a24d2e8d1
ToPresa <topresa16@hotmail.com>
2014-10-07 17:12:50 +0100

    2ºcommit
    Signed-off-by: ToPresa <topresa16@hotmail.com>

---
+++ Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "XMLScene.h"
+#include "ANFScene.h"
+using namespace std;
+
+int main(){
+	//XMLScene newS= XMLScene("demograph.xml");
+	ANFScene newS= ANFScene("teste.anf");
+	system("pause");
+	return 0;
+}
